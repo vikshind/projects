4a511ac684954b4cbd5ec6d4dbaa48a500484a32
Vikram Shinde <vikshind@bmc.com>
2022-04-02 22:36:24 +0530

    Added implementation for binarySearch and Singleton pattern

--- design-patterns/src/main/java/com/vth/pattern/singleton/Singleton.java
+++ design-patterns/src/main/java/com/vth/pattern/singleton/Singleton.java
@@ -2,4 +2,28 @@ package com.vth.pattern.singleton;
 
 public class Singleton {
 
+	private static Singleton mySingleton = null;
+
+	private Singleton() {
+	}
+
+	public static synchronized Singleton getInstance() {
+		if (mySingleton == null) {
+			mySingleton = new Singleton();
+		}
+		return mySingleton;
+	}
+
+	public static void main(String[] args) {
+		Singleton instance1 = Singleton.getInstance();
+		log(instance1.toString());
+		
+		Singleton instance2 = Singleton.getInstance();
+		log(instance2.toString());
+
+	}
+
+	public static void log(String str) {
+		System.out.println(str);
+	}
 }
